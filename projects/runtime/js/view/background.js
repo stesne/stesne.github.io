a64d8045c8183b2524a8ebc09faed7193ed64707
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-07-19 22:13:15 +0000

    Adding Project

---
+++ projects/runtime/js/view/background.js
@@ -0,0 +1,86 @@
+var background = function (window) {
+    'use strict';
+    
+    window.opspark = window.opspark || {};
+    var draw = window.opspark.draw;
+    var createjs = window.createjs;
+    
+    /*
+     * Create a background view for our game application
+     */
+    window.opspark.makeBackground = function(app,ground) {
+        if(!app) {
+            throw new Error("Invaid app argument");
+        }
+        if(!ground || typeof(ground.y) == 'undefined') {
+            throw new Error("Invalid ground argument");
+        }
+
+        // container which will be returned
+        var background;
+        
+        // ANIMATION VARIABLES HERE:
+        
+        
+        // add objects for display inb ackground
+        // called at the start of game and whenever the page is resized
+        function render() {
+            // useful variables
+            var canvasWidth = app.canvas.width;
+            var canvasHeight = app.canvas.height;
+            var groundY = ground.y;
+
+            background.removeAllChildren();
+
+            // this fills the background with a obnoxious yellow
+            // you should modify this to suit your game
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'green');
+            background.addChild(backgroundFill);
+            
+            // TODO: 3 - Add a moon and starfield
+            var moon = draw.bitmap('img/moon.png');
+            background.addChild(moon)
+            moon.x = 100;
+            moon.y = 50;
+            
+            // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+            
+            
+            // TODO 4: Part 1 - Add a tree
+            
+        }
+        
+        // Perform background animation
+        // called on each timer "tick" - 60 times per second
+        function update() {
+            // useful variables
+            var canvasWidth = app.canvas.width;
+            var canvasHeight = app.canvas.height;
+            var groundY = ground.y;
+            
+            // TODO 4: Part 2 - Move the tree!
+            
+            
+            // TODO 5: Part 2 - Parallax
+            
+
+        }
+
+        background = new createjs.Container();
+        background.resize = render;
+        background.update = update;
+        
+        app.addResizeable(background);
+        app.addUpdateable(background);
+        
+        render();
+        return background;
+    };
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = background;
+}
